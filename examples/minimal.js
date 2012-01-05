2da38edb4540a35be74e74c66a13b0c2ca79bc1c
Mike D Pilsbury <mike.pilsbury@gmail.com>
2012-01-05 21:48:26 +0000

    Change example code to use new API.

--- examples/minimal.js
+++ examples/minimal.js
@@ -1,18 +1,39 @@
 var Connection = require('../lib/tedious').Connection;
 var Request = require('../lib/tedious').Request;
 
-var options = {};
-
-var connection = new Connection('192.168.1.210', 'test', 'test', options,
-  function(err, loggedIn) {
-    // If no error, then good to go...
-    executeStatement()
+var config = {
+  server: '192.168.1.210',
+  userName: 'test',
+  password: 'test'
+  /*
+  ,options: {
+    debug: {
+      packet: true,
+      data: true,
+      payload: true,
+      token: false,
+      log: true
+    }
   }
-)
+  */
+};
+
+var connection = new Connection(config);
+
+connection.on('connection', function(err) {
+    // If no error, then good to go...
+    executeStatement();
+  }
+);
+
+connection.on('debug', function(text) {
+    //console.log(text);
+  }
+);
 
 function executeStatement() {
-  request = new Request("select 42, 'hello world'", function(err, rowCount) {
-    console.log(rowCount + ' rows returned');
+  request = new Request("select 42, 'hello world'", function(err) {
+    connection.close();
   });
 
   request.on('row', function(columns) {
@@ -23,6 +44,10 @@ function executeStatement() {
         console.log(column.value);
       }
     });
+  });
+
+  request.on('done', function(rowCount, more) {
+    console.log(rowCount + ' rows returned');
   });
 
   connection.execSql(request);
