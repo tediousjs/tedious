2657555603b8116422e28d56bfe1846e033537c6
Mike D Pilsbury <mike.pilsbury@gmail.com>
2012-01-05 21:54:50 +0000

    Change simple client example to use new API.

--- examples/simple-client.js
+++ examples/simple-client.js
@@ -12,15 +12,15 @@ config.options.debug = {
   packet: true
 }
 
-var connection = new Connection(config.server, config.userName, config.password, config.options, connected);
+var connection = new Connection(config);
 
+connection.on('connection', connected);
 connection.on('infoMessage', infoError);
 connection.on('errorMessage', infoError);
-connection.on('timeout', timeout);
-connection.on('closed', closed);
+connection.on('end', end);
 connection.on('debug', debug);
 
-function connected(err, loggedIn) {
+function connected(err) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -49,21 +49,19 @@ function exec(sql) {
   connection.execSql(request);
 }
 
-function statementComplete(err, rowCount) {
+function requestDone(rowCount, more) {
+  console.log(rowCount + ' rows');
+}
+
+function statementComplete(err) {
   if (err) {
     //console.log('Statement failed: ' + err);
-  } else {
-    console.log(rowCount + ' rows');
   }
 }
 
-function closed() {
+function end() {
   console.log('Connection closed');
   process.exit(0);
-}
-
-function timeout() {
-  console.log('Connection timeout');
 }
 
 function infoError(info) {
@@ -95,56 +93,3 @@ function row(columns) {
 
   console.log(values);
 }
-    // test.strictEqual(columnsMetadata.length, 3)
-  // )
-//   
-  // connection.on('row', (columns) ->
-
-
-  // connection.on('databaseChange', (database) ->
-    // test.strictEqual(database, config.options.database)
-  // )
-// 
-  // connection.on('debug', (message) ->
-    // #console.log(message)
-  // )
-// 
-// exports.execSimpleSql = (test) ->
-  // test.expect(15)
-// 
-  // connection = new Connection(config.server, config.userName, config.password, config.options, (err, loggedIn) ->
-    // test.ok(!err)
-    // test.ok(loggedIn)
-//     
-    // connection.execSql("select 8 as C1, 'abc' as C2, N'def' as C3", (err, rowCount) ->
-      // test.ok(!err)
-      // test.strictEqual(rowCount, 1)
-      // test.done()
-    // )
-  // )
-//   
-  // connection.on('columnMetadata', (columnsMetadata) ->
-    // test.strictEqual(columnsMetadata.length, 3)
-  // )
-//   
-  // connection.on('row', (columns) ->
-    // test.strictEqual(columns.length, 3)
-// 
-    // test.strictEqual(columns[0].value, 8)
-    // test.strictEqual(columns[1].value, 'abc')
-    // test.strictEqual(columns[2].value, 'def')
-// 
-    // test.strictEqual(columns[0].isNull, false)
-    // test.strictEqual(columns[1].isNull, false)
-    // test.strictEqual(columns[2].isNull, false)
-//     
-    // byName = columns.byName()
-    // test.strictEqual(byName.C1.value, 8)
-    // test.strictEqual(byName.C2.value, 'abc')
-    // test.strictEqual(byName.C3.value, 'def')
-  // )
-// 
-  // connection.on('debug', (message) ->
-    // #console.log(message)
-  // )
-
