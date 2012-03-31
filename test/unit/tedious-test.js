13f5508e521da4dfd63edb620e1957a5c77e3a00
Mike D Pilsbury <mike.pilsbury@gmail.com>
2012-03-31 11:55:46 +0100

    Expose data types in API.

--- test/unit/tedious-test.js
+++ test/unit/tedious-test.js
@@ -1,4 +1,12 @@
+TYPES = require('../../lib/tedious').TYPES
 Connection = require('../../lib/tedious').Connection
+
+exports.types = function(test) {
+  test.ok(TYPES)
+  test.ok(TYPES.VarChar)
+
+  test.done()
+}
 
 exports.connection = function(test) {
   test.ok(Connection)
