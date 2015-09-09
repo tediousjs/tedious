e43a465ed3db823df8e0f72562241f87e7f7e545
Arthur Schreiber <schreiber.arthur@googlemail.com>
2015-09-09 22:27:05 +0200

    Tune parsing code for different `DONE` token types.

--- src/token/done-token-parser.js
+++ src/token/done-token-parser.js
@@ -10,28 +10,47 @@ const STATUS = {
   SRVERROR: 0x0100
 };
 
+function readUInt64LE(offset, noAssert) {
+  const low = this.readUInt32LE(offset, noAssert);
+  const high = this.readUInt32LE(offset + 4, noAssert);
+
+  if (high) {
+    return Math.pow(2, 32) * high + low
+  } else {
+    return low;
+  }
+}
+
 function parseToken(parser, options, callback) {
-  parser.readUInt16LE((status) => {
+  const length = 2 + 2 + (options.tdsVersion < "7_2" ? 4 : 8);
+  const position = parser.position >>> 0;
+
+  if (position + length <= parser.buffer.length) {
+    const status = parser.buffer.readUInt16LE(position, true);
     const more = !!(status & STATUS.MORE);
     const sqlError = !!(status & STATUS.ERROR);
     const rowCountValid = !!(status & STATUS.COUNT);
     const attention = !!(status & STATUS.ATTN);
     const serverError = !!(status & STATUS.SRVERROR);
+    const curCmd = parser.buffer.readUInt16LE(position + 2, true);
+    const rowCount = (options.tdsVersion < "7_2" ? parser.buffer.readUInt32LE : readUInt64LE).call(parser.buffer, position + 4, true);
 
-    parser.readUInt16LE((curCmd) => {
-      (options.tdsVersion < "7_2" ? parser.readUInt32LE : parser.readUInt64LE).call(parser, (rowCount) => {
-        callback({
-          name: 'DONE',
-          event: 'done',
-          more: more,
-          sqlError: sqlError,
-          attention: attention,
-          serverError: serverError,
-          rowCount: rowCountValid ? rowCount : undefined,
-          curCmd: curCmd
-        });
-      });
+    parser.position = position + length;
+
+    return callback({
+      name: 'DONE',
+      event: 'done',
+      more: more,
+      sqlError: sqlError,
+      attention: attention,
+      serverError: serverError,
+      rowCount: rowCountValid ? rowCount : undefined,
+      curCmd: curCmd
     });
+  }
+
+  parser.suspend(() => {
+    parseToken(parser, options, callback);
   });
 }
 
