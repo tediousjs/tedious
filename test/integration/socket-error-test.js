c7ed09d41169f2b268f7f4779832ee00c1656dc0
Arthur Schreiber <schreiber.arthur@googlemail.com>
2015-10-23 14:54:01 +0200

    Rewrite test cases to javascript.

---
+++ test/integration/socket-error-test.js
@@ -0,0 +1,42 @@
+import Connection from '../../src/connection';
+import Request from '../../src/request';
+import fs from 'fs';
+
+function getConfig() {
+  const config = JSON.parse(fs.readFileSync(process.env.HOME + '/.tedious/test-connection.json', 'utf8')).config;
+
+  config.options.debug = {
+    packet: true,
+    data: true,
+    payload: true,
+    token: false,
+    log: true
+  };
+
+  return config;
+}
+
+exports.socketError = function(test) {
+  const connection = new Connection(getConfig());
+
+  test.expect(3);
+
+  connection.on('connect', function(err) {
+    test.ifError(err);
+
+    const request = new Request("WAITFOR 00:00:30", function(err) {
+      test.ok(~err.message.indexOf('socket error'));
+    });
+
+    connection.execSql(request);
+    connection.socket.emit('error', new Error('socket error'));
+  });
+
+  connection.on('end', function() {
+    test.done();
+  });
+
+  connection.on('error', function(err) {
+    test.ok(~err.message.indexOf('socket error'));
+  });
+};
