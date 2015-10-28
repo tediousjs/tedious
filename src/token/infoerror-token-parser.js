a82f0f8e9dc1283299e3445e5c3e47b289ec0ef0
Arthur Schreiber <schreiber.arthur@googlemail.com>
2015-10-28 13:57:22 +0100

    Hugely improve perf for `INFO` tokens.

--- src/token/infoerror-token-parser.js
+++ src/token/infoerror-token-parser.js
@@ -1,44 +1,65 @@
-function parseToken(parser, options, callback) {
-  // length
-  parser.readUInt16LE(() => {
-    parser.readUInt32LE((number) => {
-      parser.readUInt8((state) => {
-        parser.readUInt8((clazz) => {
-          parser.readUsVarChar((message) => {
-            parser.readBVarChar((serverName) => {
-              parser.readBVarChar((procName) => {
-                (options.tdsVersion < '7_2' ? parser.readUInt16LE : parser.readUInt32LE).call(parser, (lineNumber) => {
-                  callback({
-                    number: number,
-                    state: state,
-                    "class": clazz,
-                    message: message,
-                    serverName: serverName,
-                    procName: procName,
-                    lineNumber: lineNumber
-                  });
-                });
-              });
-            });
-          });
-        });
+function readUsVarChar(parser) {
+  const length = parser.buffer.readUInt16LE(parser.position, true);
+  return parser.buffer.toString("ucs2", parser.position += 2, parser.position += length * 2);
+}
+
+function readBVarChar(parser) {
+  const length = parser.buffer.readUInt8(parser.position, true);
+  return parser.buffer.toString("ucs2", parser.position += 1, parser.position += length * 2);
+}
+
+function parseToken(parser, options, name, event, callback) {
+  if (parser.position + 2 <= parser.buffer.length) {
+    const length = parser.buffer.readUInt16LE(parser.position, true);
+
+    if (parser.position + 2 + length <= parser.buffer.length) {
+      parser.position += 2;
+
+      const number = parser.buffer.readUInt32LE(parser.position, true);
+      parser.position += 4;
+
+      const state = parser.buffer.readUInt8(parser.position, true);
+      parser.position += 1;
+
+      const clazz = parser.buffer.readUInt8(parser.position, true);
+      parser.position += 1;
+
+      const message = readUsVarChar(parser);
+      const serverName = readBVarChar(parser);
+      const procName = readBVarChar(parser);
+
+      let lineNumber;
+      if (options.tdsVersion < '7_2') {
+        lineNumber = parser.buffer.readUInt16LE(parser.position, true);
+        parser.position += 2;
+      } else {
+        lineNumber = parser.buffer.readUInt32LE(parser.position, true);
+        parser.position += 4;
+      }
+
+      return callback({
+        name: name,
+        event: event,
+        number: number,
+        state: state,
+        "class": clazz,
+        message: message,
+        serverName: serverName,
+        procName: procName,
+        lineNumber: lineNumber
       });
-    });
+    }
+  }
+
+  parser.suspend(() => {
+    parseToken(parser, options, name, event, callback);
   });
 }
 
 export function infoParser(parser, colMetadata, options, callback) {
-  parseToken(parser, options, (token) => {
-    token.name = 'INFO';
-    token.event = 'infoMessage';
-    callback(token);
-  });
+  parseToken(parser, options, 'INFO', 'infoMessage', callback);
 }
 
 export function errorParser(parser, colMetadata, options, callback) {
-  parseToken(parser, options, (token) => {
-    token.name = 'ERROR';
-    token.event = 'errorMessage';
-    callback(token);
-  });
+  parseToken(parser, options, 'ERROR', 'errorMessage', callback);
 }
