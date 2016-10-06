1ea2b817df43e6e71388c8877ab2e6a43f5aef01
tvrprasad <prasadgithub@gmail.com>
2016-10-05 17:55:27 -0700

    Add dependency on lodash.clonedeep and use that for deep copy.
    Existing code is depending on lodash being brought by some other code that
    has a dependency on it. That turns out to be not reliable as on older
    versions of node, lodash is being placed in the sub-directory of the
    dependency that's bringing it in rather than at the top level
    node_modules.

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CloneDeep = require('lodash/cloneDeep');
+const CloneDeep = require('lodash.clonedeep');
 
 // A trivial wrapper so we can catch any deviations from behavior we need.
 module.exports.DeepCopy = DeepCopy;
