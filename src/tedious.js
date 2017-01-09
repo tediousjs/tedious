81cea94ac6192477a0750be60be5d225ab644ff2
Jeff Rosenberg <jeff.rosenberg@olson.com>
2017-01-08 19:48:53 -0600

    Add ConnectionError and RequestError to module.exports

--- src/tedious.js
+++ src/tedious.js
@@ -5,6 +5,9 @@ module.exports.Connection = require('./connection');
 module.exports.Request = require('./request');
 module.exports.library = require('./library');
 
+module.exports.ConnectionError = require('./errors').ConnectionError;
+module.exports.RequestError = require('./errors').RequestError;
+
 module.exports.TYPES = require('./data-type').typeByName;
 module.exports.ISOLATION_LEVEL = require('./transaction').ISOLATION_LEVEL;
 module.exports.TDS_VERSION = require('./tds-versions').versions;
