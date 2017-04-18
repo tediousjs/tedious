ceb91cf239bc17418020376351a8505a51e50d73
Arthur Schreiber <schreiber.arthur@googlemail.com>
2017-04-18 09:25:44 +0200

    test: Cover backpressure handling.

--- test/unit/message/incoming-message-stream-test.js
+++ test/unit/message/incoming-message-stream-test.js
@@ -2,7 +2,7 @@ const BufferList = require('bl');
 
 const IncomingMessageStream = require('../../../src/message/incoming-message-stream');
 const Debug = require('../../../src/debug');
-const { Packet, TYPE } = require('../../../src/packet');
+const { Packet, TYPE, HEADER_LENGTH } = require('../../../src/packet');
 
 module.exports.IncomingMessageStream = {
   'transforms data written into it into individual message streams': function(test) {
@@ -60,5 +60,76 @@ module.exports.IncomingMessageStream = {
     packet.last(true);
     packet.addData(payload.slice(3));
     incomingMessageStream.write(packet.buffer);
+  },
+
+  'correctly signals pressure building up': function(test) {
+    test.expect(33);
+
+    const debug = new Debug();
+    const incomingMessageStream = new IncomingMessageStream(debug);
+
+    // Don't actually consume any of the `IncomingMessage` data.
+    incomingMessageStream.on('data', (message) => {
+      test.strictEqual(message.type, TYPE.TABULAR_RESULT);
+    });
+
+    const packet = new Packet(TYPE.TABULAR_RESULT);
+    packet.addData(new Buffer((8 * 1024) - HEADER_LENGTH).fill('x'));
+
+    // The first packet we push in will cause a new `IncomingMessage` to be
+    // created and the packet's data will be pushed through.
+    test.ok(incomingMessageStream.write(packet.buffer));
+    test.strictEqual(0, incomingMessageStream._writableState.length);
+    test.strictEqual(0, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(8184, incomingMessageStream.currentMessage._readableState.length);
+
+    // The first packet's data will also make it to the current message.
+    test.ok(incomingMessageStream.write(packet.buffer));
+    test.strictEqual(0, incomingMessageStream._writableState.length);
+    test.strictEqual(0, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(16368, incomingMessageStream.currentMessage._readableState.length);
+
+    // The third packet's data will make us hit the `highWaterMark` in the
+    // readable side of the current message.
+    test.ok(incomingMessageStream.write(packet.buffer));
+    test.strictEqual(0, incomingMessageStream._writableState.length);
+    test.strictEqual(0, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._readableState.length);
+
+    // Now that we hit the `highWaterMark`, no more data will be transformed by
+    // the `IncomingMessage`, and data starts to buffer up on its writable side.
+    test.ok(incomingMessageStream.write(packet.buffer));
+    test.strictEqual(0, incomingMessageStream._writableState.length);
+    test.strictEqual(8184, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._readableState.length);
+
+    // More data buffering up on the writable side.
+    test.ok(incomingMessageStream.write(packet.buffer));
+    test.strictEqual(0, incomingMessageStream._writableState.length);
+    test.strictEqual(16368, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._readableState.length);
+
+    // This next packet will cause us to hit the `highWaterMark` on the writable side
+    // of the `IncomingMessage`, and `IncomingMessage#write` will start returning false.
+    // `IncomingMessageStream#_transform` will wait for the `drain` event on `IncomingMessage`,
+    // so data now also starts to buffer up on the writable side of `IncomingMessageStream`.
+    test.ok(incomingMessageStream.write(packet.buffer));
+    test.strictEqual(8192, incomingMessageStream._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._readableState.length);
+
+    // More data buffering up in `IncomingMessageStream`. `IncomingMessageStream#write`
+    // starts returning false to signal pressure building up.
+    test.ok(!incomingMessageStream.write(packet.buffer));
+    test.strictEqual(16384, incomingMessageStream._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._readableState.length);
+
+    test.ok(!incomingMessageStream.write(packet.buffer));
+    test.strictEqual(24576, incomingMessageStream._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._writableState.length);
+    test.strictEqual(24552, incomingMessageStream.currentMessage._readableState.length);
+
+    test.done();
   }
 };
