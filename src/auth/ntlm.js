85db9e60463f2a76e2f327a3dab7ddcfd2e68da4
Arthur Schreiber <arthurschreiber@github.com>
2017-10-14 13:56:50 -0700

    refactor: Move challenge parsing into `NTLMAuthProvider`.

--- src/auth/ntlm.js
+++ src/auth/ntlm.js
@@ -203,27 +203,6 @@ class NTLMResponsePayload {
   }
 }
 
-function parseChallenge(buffer) {
-  const challenge = {};
-
-  challenge.magic = buffer.slice(0, 8).toString('utf8');
-  challenge.type = buffer.readInt32LE(8);
-  challenge.domainLen = buffer.readInt16LE(12);
-  challenge.domainMax = buffer.readInt16LE(14);
-  challenge.domainOffset = buffer.readInt32LE(16);
-  challenge.flags = buffer.readInt32LE(20);
-  challenge.nonce = buffer.slice(24, 32);
-  challenge.zeroes = buffer.slice(32, 40);
-  challenge.targetLen = buffer.readInt16LE(40);
-  challenge.targetMax = buffer.readInt16LE(42);
-  challenge.targetOffset = buffer.readInt32LE(44);
-  challenge.oddData = buffer.slice(48, 56);
-  challenge.domain = buffer.slice(56, 56 + challenge.domainLen).toString('ucs2');
-  challenge.target = buffer.slice(56 + challenge.domainLen, 56 + challenge.domainLen + challenge.targetLen);
-
-  return challenge;
-}
-
 const DEFAULT_NEGOTIATE_FLAGS =
   NTLMFlags.NTLM_NegotiateUnicode +
   NTLMFlags.NTLM_NegotiateOEM +
@@ -250,8 +229,7 @@ module.exports = class NTLMAuthProvider {
       domain: this.options.domain,
       username: this.options.username,
       password: this.options.password,
-      ntlmpacket: parseChallenge(data),
-      additional: this.additional
+      ntlmpacket: this.parseChallenge(data)
     });
 
     callback(null, payload.data);
@@ -296,5 +274,26 @@ module.exports = class NTLMAuthProvider {
     buffer.writeBuffer(domainBuffer);
 
     return buffer.data;
+  }
+
+  parseChallengeMessage(buffer) {
+    const challenge = {};
+
+    challenge.magic = buffer.slice(0, 8).toString('utf8');
+    challenge.type = buffer.readInt32LE(8);
+    challenge.domainLen = buffer.readInt16LE(12);
+    challenge.domainMax = buffer.readInt16LE(14);
+    challenge.domainOffset = buffer.readInt32LE(16);
+    challenge.flags = buffer.readInt32LE(20);
+    challenge.nonce = buffer.slice(24, 32);
+    challenge.zeroes = buffer.slice(32, 40);
+    challenge.targetLen = buffer.readInt16LE(40);
+    challenge.targetMax = buffer.readInt16LE(42);
+    challenge.targetOffset = buffer.readInt32LE(44);
+    challenge.oddData = buffer.slice(48, 56);
+    challenge.domain = buffer.slice(56, 56 + challenge.domainLen).toString('ucs2');
+    challenge.target = buffer.slice(56 + challenge.domainLen, 56 + challenge.domainLen + challenge.targetLen);
+
+    return challenge;
   }
 };
