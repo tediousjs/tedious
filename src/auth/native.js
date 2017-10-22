38ed20fcedd58bc0a85dad09a967ca6481c23d39
Arthur Schreiber <arthurschreiber@github.com>
2017-10-22 13:48:21 +0200

    refactor: Remove `sspiClientResponsePending` check.
    We only perform handshaking after receiving a message, and no further TDS messages should be received during the handshake, so there is no need to do an explicit check in the auth provider.

--- src/auth/native.js
+++ src/auth/native.js
@@ -9,27 +9,17 @@ class NativeAuthProvider {
   }
 
   handshake(data, callback) {
-    if (this.sspiClientResponsePending) {
-      // We got data from the server while we're waiting for getNextBlob()
-      // call to complete on the client. We cannot process server data
-      // until this call completes as the state can change on completion of
-      // the call. Queue it for later.
-      const boundDispatchEvent = this.connection.dispatchEvent.bind(this.connection);
-      return setImmediate(boundDispatchEvent, 'message');
-    }
-
     if (data) {
-      this.sspiClientResponsePending = true;
       return this.client.getNextBlob(data, 0, data.length, (responseBuffer, isDone, errorCode, errorString) => {
         if (errorCode) {
           return callback(new Error(errorString));
         }
-        this.sspiClientResponsePending = false;
+
         callback(null, responseBuffer);
       });
-    }
-    else {
+    } else {
       const server = this.connection.routingData ? this.connection.routingData.server : this.connection.config.server;
+
       Fqdn.getFqdn(server, (err, fqdn) => {
         if (err) {
           return callback(new Error('Error getting Fqdn. Error details: ' + err.message));
@@ -37,7 +27,6 @@ class NativeAuthProvider {
 
         const spn = MakeSpn.makeSpn('MSSQLSvc', fqdn, this.connection.config.options.port);
         this.client = new SspiClientApi.SspiClient(spn, this.connection.config.securityPackage);
-        this.sspiClientResponsePending = true;
         this.client.getNextBlob(null, 0, 0, (responseBuffer, isDone, errorCode, errorString) => {
           if (errorCode) {
             return callback(new Error(errorString));
@@ -46,7 +35,7 @@ class NativeAuthProvider {
           if (isDone) {
             return callback(new Error('Unexpected isDone=true on getNextBlob in sendLogin7Packet.'));
           }
-          this.sspiClientResponsePending = false;
+
           callback(null, responseBuffer);
         });
       });
