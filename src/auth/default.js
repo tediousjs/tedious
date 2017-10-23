1e723c14a196c100ff8371b6103397bad67f173f
Arthur Schreiber <arthurschreiber@github.com>
2017-10-23 09:14:16 +0200

    docs: More documentation for the default auth provider.

--- src/auth/default.js
+++ src/auth/default.js
@@ -2,6 +2,9 @@ const EMPTY_BUFFER = new Buffer(0);
 
 /**
   Authenticate to SQL Server via built-in authentication.
+
+  @param {Connection} connection
+  @param {Object} options
 */
 class DefaultAuthProvider {
   constructor(connection, options) {
@@ -9,6 +12,20 @@ class DefaultAuthProvider {
     this.options = options;
   }
 
+  /*
+    Perform authentication handshakes.
+
+    This method will be called one or multiple times during the authentication
+    handshake procedure. The `data` parameter can either be `null`
+    (to get the initial client handshake) or a `Buffer` containing
+    authentication data from the server.
+
+    `callback` should be called asynchronously with an error or
+    with the data to be sent back to the server.
+
+    @param {?Buffer}
+    @param {Function} callback
+  */
   handshake(data, callback) {
     process.nextTick(callback, null, EMPTY_BUFFER);
   }
