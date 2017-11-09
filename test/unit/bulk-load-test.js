6bf2a132abf7e29db20435356f8e0cefdfc84818
Suraiya Hameed <v-suhame@microsoft.com>
2017-11-09 15:11:06 -0800

    update test to reflect code change

--- test/unit/bulk-load-test.js
+++ test/unit/bulk-load-test.js
@@ -1,9 +1,8 @@
 var BulkLoad = require('../../src/bulk-load');
 
 exports['test invalid bulkOptions'] = function(test) {
-  test.expect(3);
+  test.expect(2);
   const bulkLoad = new BulkLoad();
-  test.throws(() => bulkLoad.setOptions({ keepNull: true }), TypeError, 'No Exception thrown for invalid bulk load option');
   test.throws(() => bulkLoad.setOptions(), TypeError, 'No Exception thrown for empty options to bulkLoad');
   test.throws(() => bulkLoad.setOptions('garbageText'), TypeError, 'No Exception thrown for invalid bulk load option');
   test.done();
