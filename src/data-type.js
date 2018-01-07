12b36290c15cd2be021b95e4bbb817547574171d
Arthur Schreiber <schreiber.arthur@googlemail.com>
2018-01-07 14:17:11 +0100

    fix: Be stricter about the type of `parameter.value` for `VarBinary`.

--- src/data-type.js
+++ src/data-type.js
@@ -959,19 +959,27 @@ const TYPE = module.exports.TYPE = {
     },
 
     writeParameterData: function(buffer, parameter) {
-      if (parameter.value != null) {
-        if (parameter.length <= this.maximumLength) {
-          buffer.writeUsVarbyte(parameter.value);
-        } else {
-          buffer.writePLPBody(parameter.value);
-        }
-      } else {
+      const value = parameter.value;
+
+      if (value === undefined || value === null) {
         if (parameter.length <= this.maximumLength) {
           buffer.writeUInt16LE(NULL);
         } else {
           buffer.writeUInt32LE(0xFFFFFFFF);
           buffer.writeUInt32LE(0xFFFFFFFF);
         }
+
+        return;
+      }
+
+      if (typeof value !== 'string' && !(value instanceof Buffer)) {
+        throw new TypeError(`parameter.value must be a Buffer, string, undefined or null. Received type ${typeof value}`);
+      }
+
+      if (parameter.length <= this.maximumLength) {
+        buffer.writeUsVarbyte(parameter.value);
+      } else {
+        buffer.writePLPBody(parameter.value);
       }
     },
 
