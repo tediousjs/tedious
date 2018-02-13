6cfa3cd2b6fbc7a053cc2b54b79b6248f8272dbb
Hadis-Fard <v-hakaka@microsoft.com>
2018-02-13 11:08:07 -0800

    chore: fix else block indentations

--- src/connection.js
+++ src/connection.js
@@ -1711,8 +1711,7 @@ Connection.prototype.STATE = {
               return this.transitionTo(this.STATE.SENT_LOGIN7_WITH_FEDAUTH);
             } else if (this.config.domain) {
               return this.transitionTo(this.STATE.SENT_LOGIN7_WITH_NTLM);
-            }
-            else {
+            } else {
               return this.transitionTo(this.STATE.SENT_LOGIN7_WITH_STANDARD_LOGIN);
             }
           });
