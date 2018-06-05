3e07782bf308be7d3591e755cfe429337f45e8af
Hadis-Fard <v-hakaka@microsoft.com>
2018-06-04 18:30:47 -0700

    refactor: test multiple features

--- test/unit/token/feature-ext-parser-test.js
+++ test/unit/token/feature-ext-parser-test.js
@@ -4,13 +4,21 @@ var WritableTrackingBuffer = require('../../../src/tracking-buffer/writable-trac
 
 module.exports.fedauth = function(test) {
   var buffer = new WritableTrackingBuffer(50, 'ucs2');
-  const featureId = 0x02;
-  const featureAckDataLen = 0;
 
   buffer.writeUInt8(0xAE); // FEATUREEXTACK token header
-  buffer.writeUInt8(featureId);
-  buffer.writeUInt32LE(featureAckDataLen);
-  buffer.writeBuffer(new Buffer(0));
+
+  buffer.writeUInt8(0x01);
+  buffer.writeUInt32LE(1);
+  buffer.writeBuffer(Buffer.from('a'));
+
+  buffer.writeUInt8(0x02);
+  buffer.writeUInt32LE(2);
+  buffer.writeBuffer(Buffer.from('bc'));
+
+  buffer.writeUInt8(0x03);
+  buffer.writeUInt32LE(0);
+  buffer.writeBuffer(Buffer.from(''));
+
   buffer.writeUInt8(0xFF); // terminator
 
   var parser = new Parser({token() {}}, {}, {});
@@ -18,8 +26,9 @@ module.exports.fedauth = function(test) {
 
   var token = parser.read();
 
-  test.strictEqual(token.featureId, featureId);
-  test.strictEqual(token.featureAckDataLen, featureAckDataLen);
+  test.ok(token.featureAckOpts[0x01].equals(Buffer.from('a')));
+  test.ok(token.featureAckOpts[0x02].equals(Buffer.from('bc')));
+  test.ok(token.featureAckOpts[0x03].equals(Buffer.from('')));
 
   test.done();
 };
