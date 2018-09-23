6b0ef1d3ab0a894abf104944c6543792e2a1f205
Igor Savin <iselwin@gmail.com>
2018-09-23 14:09:30 +0200

    Fix import

--- src/ntlm-payload.js
+++ src/ntlm-payload.js
@@ -2,7 +2,7 @@
 
 const WritableTrackingBuffer = require('./tracking-buffer/writable-tracking-buffer');
 const crypto = require('crypto');
-const BigInteger = require('big-number').n;
+const BigInteger = require('big-number');
 
 const hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
