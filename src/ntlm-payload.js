9def104da14db657b7276408e331bb96e0980bcc
Igor Savin <iselwin@gmail.com>
2018-09-23 17:37:25 +0200

    chore: upgrade to `big-number@1.0.0`

--- src/ntlm-payload.js
+++ src/ntlm-payload.js
@@ -2,7 +2,7 @@
 
 const WritableTrackingBuffer = require('./tracking-buffer/writable-tracking-buffer');
 const crypto = require('crypto');
-const BigInteger = require('big-number').n;
+const BigInteger = require('big-number');
 
 const hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
@@ -112,7 +112,7 @@ class NTLMResponsePayload {
   }
 
   createTimestamp(time: number) {
-    const tenthsOfAMicrosecond = new BigInteger(time).plus(11644473600).multiply(10000000);
+    const tenthsOfAMicrosecond = BigInteger(time).plus(11644473600).multiply(10000000);
     const hexArray = [];
 
     let pair = [];
