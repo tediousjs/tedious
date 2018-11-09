48e33288ca2a337faaf2d76996df2c240bd26cb5
heroboy <ywqywq@yeah.net>
2018-11-09 10:21:11 +0800

    fix for linter

--- src/tracking-buffer/writable-tracking-buffer.js
+++ src/tracking-buffer/writable-tracking-buffer.js
@@ -274,8 +274,8 @@ module.exports = class WritableTrackingBuffer {
 
   writeMoney(value: number) {
     //floor(-1.9) === -2
-    //-1.9|0 === -1 
-    this.writeInt32LE((value * SHIFT_RIGHT_32)|0);
+    //-1.9|0 === -1
+    this.writeInt32LE((value * SHIFT_RIGHT_32) | 0);
     this.writeInt32LE(value & -1);
   }
 };
--- test/unit/money-type-test.js
+++ test/unit/money-type-test.js
@@ -1,38 +1,38 @@
 var Money = require('../../src/data-types/money');
 var WritableTrackingBuffer = require('../../src/tracking-buffer/writable-tracking-buffer');
 
-exports.TestValidMoneyValue = function (test)
+exports.TestValidMoneyValue = function(test)
 {
-	let validValues = [-922337203685477.5, 922337203685477.5,
-	-922337203685477.6 + 0.2, 922337203685477.6 - 0.2, 0, null]
-	for (let value of validValues)
-	{
-		let value2 = Money.validate(value);
-		test.equal(value, value2);
+  const validValues = [-922337203685477.5, 922337203685477.5,
+    -922337203685477.6 + 0.2, 922337203685477.6 - 0.2, 0, null];
+  for (const value of validValues)
+  {
+    const value2 = Money.validate(value);
+    test.equal(value, value2);
 
-		test.doesNotThrow(function ()
-		{
-			let value2 = Money.validate(value);
-			test.equal(value, value2);
-		});
+    test.doesNotThrow(function()
+    {
+      const value2 = Money.validate(value);
+      test.equal(value, value2);
+    });
 
-		test.doesNotThrow(function ()
-		{
-			let buf = new WritableTrackingBuffer(8);
-			Money.writeParameterData(buf, { value: value });
-		});
-	}
-	test.done();
-}
+    test.doesNotThrow(function()
+    {
+      const buf = new WritableTrackingBuffer(8);
+      Money.writeParameterData(buf, { value: value });
+    });
+  }
+  test.done();
+};
 
 exports.TestInvalidMoneyValue = function(test)
 {
-	let invalidValues = [Number.NaN,Number.POSITIVE_INFINITY,Number.NEGATIVE_INFINITY,922337203685477.6,922337203685477.6];
-	for(let value of invalidValues)
-	{
-		let ret = Money.validate(value);
-		test.ok(ret instanceof TypeError);
-	}
-	test.done();
-}
+  const invalidValues = [Number.NaN, Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY, 922337203685477.6, 922337203685477.6];
+  for (const value of invalidValues)
+  {
+    const ret = Money.validate(value);
+    test.ok(ret instanceof TypeError);
+  }
+  test.done();
+};
 
