a402c24891ec76583497b88897f0f5f6d9ffbb7e
MichaelSun90 <v-mutsun@microsoft.com>
2019-06-06 10:14:41 -0700

    test: added tests for slash separated and wrong input

--- test/unit/data-type.js
+++ test/unit/data-type.js
@@ -74,10 +74,71 @@ describe('Data Types', function() {
     ]) {
       const parameter = { value: value, scale: scale };
       parameter.value.nanosecondDelta = nanosecondDelta;
-
       const buffer = new WritableTrackingBuffer(16);
       type.writeParameterData(buffer, parameter, { useUTC: false }, () => {});
       assert.deepEqual(buffer.data, expectedBuffer);
     }
   });
 });
+// Test rounding of nanosecondDelta
+exports.nanoSecondRounding = function(test) {
+  const type = TYPES.typeByName.Time;
+  for (const [value, nanosecondDelta, scale, expectedBuffer] of [
+    [new Date(2017, 6, 29, 17, 20, 3, 503), 0.0006264, 7, Buffer.from('0568fc624b91', 'hex')],
+    [new Date(2017, 9, 1, 1, 31, 4, 12), 0.0004612, 7, Buffer.from('05c422ceb80c', 'hex')],
+    [new Date(2017, 7, 3, 12, 52, 28, 373), 0.0007118, 7, Buffer.from('051e94c8e96b', 'hex')]
+  ]) {
+    const parameter = { value: value, scale: scale };
+    parameter.value.nanosecondDelta = nanosecondDelta;
+
+    const buffer = new WritableTrackingBuffer(16);
+    type.writeParameterData(buffer, parameter, { useUTC: false });
+
+    test.deepEqual(buffer.data, expectedBuffer);
+  }
+  test.done();
+};
+
+exports.dateInputWithSlashSeparator = function(test) {
+  const type = TYPES.typeByName.Date;
+  var buffer = new WritableTrackingBuffer(8);
+  var parameter = { value: '2015/06/18 23:59:59' };
+  type.writeParameterData(buffer, parameter, { useUTC: false });
+  test.deepEqual(buffer.data, Buffer.from('03163a0b', 'hex'));
+  test.done();
+};
+
+exports.dateTimeInputWithSlashSeparator = function(test) {
+  const type = TYPES.typeByName.DateTime;
+  var buffer = new WritableTrackingBuffer(16);
+  var parameter = { value: '2015/06/20' };
+  type.writeParameterData(buffer, parameter, { useUTC: false });
+  test.strictEqual(buffer.buffer.readInt32LE(1), 42173);
+  test.done();
+};
+
+// Test if we passed in an invalid date input, the writeParameterData should throw a corresponding error
+exports.dateTypeWithErrorInput = function(test) {
+  const type = TYPES.typeByName.Date;
+  var buffer = new WritableTrackingBuffer(8);
+  var parameter = { value: 'wrongvalue' };
+  try {
+    type.writeParameterData(buffer, parameter, { useUTC: false });
+  } catch (err) {
+    test.strictEqual(err.message, 'Invalid date.');
+  }
+  test.done();
+};
+
+// Test if we passed in an invalid date input, the writeParameterData should throw a corresponding error
+exports.dateTimeTypeWithErrorInput = function(test) {
+  const type = TYPES.typeByName.DateTime;
+  var buffer = new WritableTrackingBuffer(8);
+  var parameter = { value: 'wrongvalue' };
+  try {
+    type.writeParameterData(buffer, parameter, { useUTC: false });
+  } catch (err) {
+    test.strictEqual(err.message, 'Invalid date.');
+  }
+  test.done();
+};
