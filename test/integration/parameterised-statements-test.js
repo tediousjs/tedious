480c870d57edebea7b4490f49bc757d9dd3cb15b
IanChokSimba <ichok@magnitude.com>
2019-08-06 11:28:52 -0700

    fix: add timeout for tests

--- test/integration/parameterised-statements-test.js
+++ test/integration/parameterised-statements-test.js
@@ -139,6 +139,8 @@ function execSqlOutput(done, type, value, expectedValue) {
 }
 
 describe('Parameterised Statements Test', function() {
+  this.timeout(60000)
+
   it('should test bit True', function(done) {
     execSql(done, TYPES.Bit, true);
   });
