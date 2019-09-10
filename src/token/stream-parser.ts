19c50ad26883ade8d9b5156c9fb683c17c1d55a1
Arthur Schreiber <arthurschreiber@github.com>
2019-09-10 16:18:14 -0300

    refactor: move token type information around

--- src/token/stream-parser.ts
+++ src/token/stream-parser.ts
@@ -2,7 +2,7 @@ import Debug from '../debug';
 import { ConnectionOptions } from '../connection';
 
 const Transform = require('readable-stream').Transform;
-const TYPE = require('./token').TYPE;
+import { TYPE, Token } from './token';
 
 const tokenParsers: {
   [token: number]: (parser: Parser, colMetadata: any, options: ConnectionOptions, done: (token: Token) => void) => void
@@ -25,33 +25,6 @@ tokenParsers[TYPE.NBCROW] = require('./nbcrow-token-parser');
 tokenParsers[TYPE.SSPI] = require('./sspi-token-parser');
 
 class EndOfMessageMarker {}
-
-type Token = { name: 'COLMETADATA', event: 'columnMetadata', columns: any[] }
-           | { name: 'DONE', event: 'done' }
-           | { name: 'DONEINPROC', event: 'doneInProc' }
-           | { name: 'DONEPROC', event: 'doneProc' }
-           | { name: 'ENVCHANGE', type: 'DATABASE', event: 'databaseChange', oldValue: string, newValue: string }
-           | { name: 'ENVCHANGE', type: 'LANGUAGE', event: 'languageChange', oldValue: string, newValue: string }
-           | { name: 'ENVCHANGE', type: 'CHARSET', event: 'charsetChange', oldValue: string, newValue: string }
-           | { name: 'ENVCHANGE', type: 'PACKET_SIZE', event: 'packetSizeChange', oldValue: number, newValue: number }
-           | { name: 'ENVCHANGE', type: 'BEGIN_TXN', event: 'beginTransaction', oldValue: Buffer, newValue: Buffer }
-           | { name: 'ENVCHANGE', type: 'COMMIT_TXN', event: 'commitTransaction', oldValue: Buffer, newValue: Buffer }
-           | { name: 'ENVCHANGE', type: 'ROLLBACK_TXN', event: 'rollbackTransaction', oldValue: Buffer, newValue: Buffer }
-           | { name: 'ENVCHANGE', type: 'DATABASE_MIRRORING_PARTNER', event: 'partnerNode', oldValue: string, newValue: string }
-           | { name: 'ENVCHANGE', type: 'RESET_CONNECTION', event: 'resetConnection', oldValue: Buffer, newValue: Buffer }
-           | { name: 'ENVCHANGE', type: 'ROUTING_CHANGE', event: 'routingChange', oldValue: Buffer, newValue: { protocol: number, port: number, server: string } }
-           | { name: 'FEATUREEXTACK', event: 'featureExtAck', fedAuth?: Buffer }
-           | { name: 'FEDAUTHINFO', event: 'fedAuthInfo', spn?: string, stsurl?: string }
-           | { name: 'INFO', event: 'infoMessage', number: number, state: number, class: number, message: string, serverName: string, procName: string, lineNumber: number }
-           | { name: 'ERROR', event: 'errorMessage', number: number, state: number, class: number, message: string, serverName: string, procName: string, lineNumber: number }
-           | { name: 'LOGINACK', event: 'loginack', interface: string, tdsVersion: string, progName: string, progVersion: { major: number, minor: number, buildNumHi: number, buildNumLow: number } }
-           | { name: 'NBCROW', event: 'row', columns: any[] }
-           | { name: 'ORDER', event: 'order', orderColumns: number[] }
-           | { name: 'RETURNSTATUS', event: 'returnStatus', value: number }
-           | { name: 'RETURNVALUE', event: 'returnValue', paramOrdinal: number, paramName: string, metadata: any, value: unknown }
-           | { name: 'ROW', event: 'row', columns: any }
-           | { name: 'SSPICHALLENGE', event: 'sspichallenge', ntlmpacket: any, ntlmpacketBuffer: Buffer }
-           | { name: 'EOM', event: 'endOfMessage' };
 
 export default class Parser extends Transform {
   debug: Debug;
--- src/token/token.ts
+++ src/token/token.ts
@@ -21,3 +21,30 @@ export const TYPE = {
   SSPI: 0xED,
   TABNAME: 0xA4
 };
+
+export type Token = { name: 'COLMETADATA', event: 'columnMetadata', columns: any[] }
+                  | { name: 'DONE', event: 'done' }
+                  | { name: 'DONEINPROC', event: 'doneInProc' }
+                  | { name: 'DONEPROC', event: 'doneProc' }
+                  | { name: 'ENVCHANGE', type: 'DATABASE', event: 'databaseChange', oldValue: string, newValue: string }
+                  | { name: 'ENVCHANGE', type: 'LANGUAGE', event: 'languageChange', oldValue: string, newValue: string }
+                  | { name: 'ENVCHANGE', type: 'CHARSET', event: 'charsetChange', oldValue: string, newValue: string }
+                  | { name: 'ENVCHANGE', type: 'PACKET_SIZE', event: 'packetSizeChange', oldValue: number, newValue: number }
+                  | { name: 'ENVCHANGE', type: 'BEGIN_TXN', event: 'beginTransaction', oldValue: Buffer, newValue: Buffer }
+                  | { name: 'ENVCHANGE', type: 'COMMIT_TXN', event: 'commitTransaction', oldValue: Buffer, newValue: Buffer }
+                  | { name: 'ENVCHANGE', type: 'ROLLBACK_TXN', event: 'rollbackTransaction', oldValue: Buffer, newValue: Buffer }
+                  | { name: 'ENVCHANGE', type: 'DATABASE_MIRRORING_PARTNER', event: 'partnerNode', oldValue: string, newValue: string }
+                  | { name: 'ENVCHANGE', type: 'RESET_CONNECTION', event: 'resetConnection', oldValue: Buffer, newValue: Buffer }
+                  | { name: 'ENVCHANGE', type: 'ROUTING_CHANGE', event: 'routingChange', oldValue: Buffer, newValue: { protocol: number, port: number, server: string } }
+                  | { name: 'FEATUREEXTACK', event: 'featureExtAck', fedAuth?: Buffer }
+                  | { name: 'FEDAUTHINFO', event: 'fedAuthInfo', spn?: string, stsurl?: string }
+                  | { name: 'INFO', event: 'infoMessage', number: number, state: number, class: number, message: string, serverName: string, procName: string, lineNumber: number }
+                  | { name: 'ERROR', event: 'errorMessage', number: number, state: number, class: number, message: string, serverName: string, procName: string, lineNumber: number }
+                  | { name: 'LOGINACK', event: 'loginack', interface: string, tdsVersion: string, progName: string, progVersion: { major: number, minor: number, buildNumHi: number, buildNumLow: number } }
+                  | { name: 'NBCROW', event: 'row', columns: any[] }
+                  | { name: 'ORDER', event: 'order', orderColumns: number[] }
+                  | { name: 'RETURNSTATUS', event: 'returnStatus', value: number }
+                  | { name: 'RETURNVALUE', event: 'returnValue', paramOrdinal: number, paramName: string, metadata: any, value: unknown }
+                  | { name: 'ROW', event: 'row', columns: any }
+                  | { name: 'SSPICHALLENGE', event: 'sspichallenge', ntlmpacket: any, ntlmpacketBuffer: Buffer }
+                  | { name: 'EOM', event: 'endOfMessage' };
