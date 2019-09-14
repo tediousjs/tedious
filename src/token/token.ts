5d3b050a9a163b08c65a9832882748f7158937e4
Arthur Schreiber <arthurschreiber@github.com>
2019-09-14 20:25:43 -0300

    refactor: cleanup token definitions

--- src/token/token.ts
+++ src/token/token.ts
@@ -25,27 +25,29 @@ export const TYPE = {
   TABNAME: 0xA4
 };
 
-export abstract class BaseToken {}
+export abstract class Token {
+  name: string;
+  event: string;
 
-export class ColMetadataToken extends BaseToken {
-  name: 'COLMETADATA';
-  event: 'columnMetadata';
+  constructor(name: string, event: string) {
+    this.name = name;
+    this.event = event;
+  }
+}
 
+export class ColMetadataToken extends Token {
   columns: ColumnMetadata[]
 
   constructor(columns: ColumnMetadata[]) {
-    super();
-
-    this.name = 'COLMETADATA';
-    this.event = 'columnMetadata';
+    super('COLMETADATA', 'columnMetadata');
 
     this.columns = columns;
   }
 }
 
-export class DoneToken extends BaseToken {
-  name: 'DONE';
-  event: 'done';
+export class DoneToken extends Token {
+  name!: 'DONE';
+  event!: 'done';
 
   more: boolean;
   sqlError: boolean;
@@ -55,10 +57,7 @@ export class DoneToken extends BaseToken {
   curCmd: number;
 
   constructor({ more, sqlError, attention, serverError, rowCount, curCmd }: { more: boolean, sqlError: boolean, attention: boolean, serverError: boolean, rowCount: number | undefined, curCmd: number }) {
-    super();
-
-    this.name = 'DONE';
-    this.event = 'done';
+    super('DONE', 'done');
 
     this.more = more;
     this.sqlError = sqlError;
@@ -69,10 +68,7 @@ export class DoneToken extends BaseToken {
   }
 }
 
-export class DoneInProcToken extends BaseToken {
-  name: 'DONEINPROC';
-  event: 'doneInProc';
-
+export class DoneInProcToken extends Token {
   more: boolean;
   sqlError: boolean;
   attention: boolean;
@@ -81,10 +77,7 @@ export class DoneInProcToken extends BaseToken {
   curCmd: number;
 
   constructor({ more, sqlError, attention, serverError, rowCount, curCmd }: { more: boolean, sqlError: boolean, attention: boolean, serverError: boolean, rowCount: number | undefined, curCmd: number }) {
-    super();
-
-    this.name = 'DONEINPROC';
-    this.event = 'doneInProc';
+    super('DONEINPROC', 'doneInProc');
 
     this.more = more;
     this.sqlError = sqlError;
@@ -95,10 +88,7 @@ export class DoneInProcToken extends BaseToken {
   }
 }
 
-export class DoneProcToken extends BaseToken {
-  name: 'DONEPROC';
-  event: 'doneProc';
-
+export class DoneProcToken extends Token {
   more: boolean;
   sqlError: boolean;
   attention: boolean;
@@ -107,10 +97,7 @@ export class DoneProcToken extends BaseToken {
   curCmd: number;
 
   constructor({ more, sqlError, attention, serverError, rowCount, curCmd }: { more: boolean, sqlError: boolean, attention: boolean, serverError: boolean, rowCount: number | undefined, curCmd: number }) {
-    super();
-
-    this.name = 'DONEPROC';
-    this.event = 'doneProc';
+    super('DONEPROC', 'doneProc');
 
     this.more = more;
     this.sqlError = sqlError;
@@ -121,19 +108,13 @@ export class DoneProcToken extends BaseToken {
   }
 }
 
-export class DatabaseEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'databaseChange';
-
+export class DatabaseEnvChangeToken extends Token {
   type: 'DATABASE';
   newValue: string;
   oldValue: string;
 
   constructor(newValue: string, oldValue: string) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'databaseChange';
+    super('ENVCHANGE', 'databaseChange');
 
     this.type = 'DATABASE';
     this.newValue = newValue;
@@ -141,19 +122,13 @@ export class DatabaseEnvChangeToken extends BaseToken {
   }
 }
 
-export class LanguageEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'languageChange';
-
+export class LanguageEnvChangeToken extends Token {
   type: 'LANGUAGE';
   newValue: string;
   oldValue: string;
 
   constructor(newValue: string, oldValue: string) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'languageChange';
+    super('ENVCHANGE', 'languageChange');
 
     this.type = 'LANGUAGE';
     this.newValue = newValue;
@@ -161,19 +136,13 @@ export class LanguageEnvChangeToken extends BaseToken {
   }
 }
 
-export class CharsetEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'charsetChange';
-
+export class CharsetEnvChangeToken extends Token {
   type: 'CHARSET';
   newValue: string;
   oldValue: string;
 
   constructor(newValue: string, oldValue: string) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'charsetChange';
+    super('ENVCHANGE', 'charsetChange');
 
     this.type = 'CHARSET';
     this.newValue = newValue;
@@ -181,19 +150,13 @@ export class CharsetEnvChangeToken extends BaseToken {
   }
 }
 
-export class PacketSizeEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'packetSizeChange';
-
+export class PacketSizeEnvChangeToken extends Token {
   type: 'PACKET_SIZE';
   newValue: number;
   oldValue: number;
 
   constructor(newValue: number, oldValue: number) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'packetSizeChange';
+    super('ENVCHANGE', 'packetSizeChange');
 
     this.type = 'PACKET_SIZE';
     this.newValue = newValue;
@@ -201,16 +164,13 @@ export class PacketSizeEnvChangeToken extends BaseToken {
   }
 }
 
-export class BeginTransactionEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'beginTransaction';
-
+export class BeginTransactionEnvChangeToken extends Token {
   type: 'BEGIN_TXN';
   newValue: Buffer;
   oldValue: Buffer;
 
   constructor(newValue: Buffer, oldValue: Buffer) {
-    super();
+    super('ENVCHANGE', 'beginTransaction');
 
     this.name = 'ENVCHANGE';
     this.event = 'beginTransaction';
@@ -221,19 +181,13 @@ export class BeginTransactionEnvChangeToken extends BaseToken {
   }
 }
 
-export class CommitTransactionEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'commitTransaction';
-
+export class CommitTransactionEnvChangeToken extends Token {
   type: 'COMMIT_TXN';
   newValue: Buffer;
   oldValue: Buffer;
 
   constructor(newValue: Buffer, oldValue: Buffer) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'commitTransaction';
+    super('ENVCHANGE', 'commitTransaction');
 
     this.type = 'COMMIT_TXN';
     this.newValue = newValue;
@@ -241,19 +195,13 @@ export class CommitTransactionEnvChangeToken extends BaseToken {
   }
 }
 
-export class RollbackTransactionEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'rollbackTransaction';
-
+export class RollbackTransactionEnvChangeToken extends Token {
   type: 'ROLLBACK_TXN';
   oldValue: Buffer;
   newValue: Buffer;
 
   constructor(newValue: Buffer, oldValue: Buffer) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'rollbackTransaction';
+    super('ENVCHANGE', 'rollbackTransaction');
 
     this.type = 'ROLLBACK_TXN';
     this.newValue = newValue;
@@ -261,16 +209,13 @@ export class RollbackTransactionEnvChangeToken extends BaseToken {
   }
 }
 
-export class DatabaseMirroringPartnerEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'partnerNode';
-
+export class DatabaseMirroringPartnerEnvChangeToken extends Token {
   type: 'DATABASE_MIRRORING_PARTNER';
   oldValue: string;
   newValue: string;
 
   constructor(newValue: string, oldValue: string) {
-    super();
+    super('ENVCHANGE', 'partnerNode');
 
     this.name = 'ENVCHANGE';
     this.event = 'partnerNode';
@@ -281,16 +226,13 @@ export class DatabaseMirroringPartnerEnvChangeToken extends BaseToken {
   }
 }
 
-export class ResetConnectionEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'resetConnection';
-
+export class ResetConnectionEnvChangeToken extends Token {
   type: 'RESET_CONNECTION';
   oldValue: Buffer;
   newValue: Buffer;
 
   constructor(newValue: Buffer, oldValue: Buffer) {
-    super();
+    super('ENVCHANGE', 'resetConnection');
 
     this.name = 'ENVCHANGE';
     this.event = 'resetConnection';
@@ -301,19 +243,13 @@ export class ResetConnectionEnvChangeToken extends BaseToken {
   }
 }
 
-export class CollationChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'sqlCollationChange';
-
+export class CollationChangeToken extends Token {
   type: 'SQL_COLLATION';
-  oldValue: string;
-  newValue: string;
+  oldValue: Buffer;
+  newValue: Buffer;
 
-  constructor(newValue: string, oldValue: string) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'sqlCollationChange';
+  constructor(newValue: Buffer, oldValue: Buffer) {
+    super('ENVCHANGE', 'sqlCollationChange');
 
     this.type = 'SQL_COLLATION';
     this.newValue = newValue;
@@ -321,19 +257,13 @@ export class CollationChangeToken extends BaseToken {
   }
 }
 
-export class RoutingEnvChangeToken extends BaseToken {
-  name: 'ENVCHANGE';
-  event: 'routingChange';
-
+export class RoutingEnvChangeToken extends Token {
   type: 'ROUTING_CHANGE';
   newValue: { protocol: number, port: number, server: string };
   oldValue: Buffer;
 
   constructor(newValue: { protocol: number, port: number, server: string }, oldValue: Buffer) {
-    super();
-
-    this.name = 'ENVCHANGE';
-    this.event = 'routingChange';
+    super('ENVCHANGE', 'routingChange');
 
     this.type = 'ROUTING_CHANGE';
     this.newValue = newValue;
@@ -341,44 +271,29 @@ export class RoutingEnvChangeToken extends BaseToken {
   }
 }
 
-export class FeatureExtAckToken extends BaseToken {
-  name: 'FEATUREEXTACK';
-  event: 'featureExtAck';
-
+export class FeatureExtAckToken extends Token {
   fedAuth: Buffer | undefined;
 
   constructor(fedAuth: Buffer | undefined) {
-    super();
-
-    this.name = 'FEATUREEXTACK';
-    this.event = 'featureExtAck';
+    super('FEATUREEXTACK', 'featureExtAck');
 
     this.fedAuth = fedAuth;
   }
 }
 
-export class FedAuthInfoToken extends BaseToken {
-  name: 'FEDAUTHINFO';
-  event: 'fedAuthInfo';
-
+export class FedAuthInfoToken extends Token {
   spn: string | undefined;
   stsurl: string | undefined;
 
   constructor(spn: string | undefined, stsurl: string | undefined) {
-    super();
-
-    this.name = 'FEDAUTHINFO';
-    this.event = 'fedAuthInfo';
+    super('FEDAUTHINFO', 'fedAuthInfo');
 
     this.spn = spn;
     this.stsurl = stsurl;
   }
 }
 
-export class InfoMessageToken extends BaseToken {
-  name: 'INFO';
-  event: 'infoMessage';
-
+export class InfoMessageToken extends Token {
   number: number;
   state: number;
   class: number;
@@ -388,10 +303,7 @@ export class InfoMessageToken extends BaseToken {
   lineNumber: number;
 
   constructor({ number, state, class: clazz, message, serverName, procName, lineNumber }: { number: number, state: number, class: number, message: string, serverName: string, procName: string, lineNumber: number }) {
-    super();
-
-    this.name = 'INFO';
-    this.event = 'infoMessage';
+    super('INFO', 'infoMessage');
 
     this.number = number;
     this.state = state;
@@ -403,10 +315,7 @@ export class InfoMessageToken extends BaseToken {
   }
 }
 
-export class ErrorMessageToken extends BaseToken {
-  name: 'ERROR';
-  event: 'errorMessage';
-
+export class ErrorMessageToken extends Token {
   number: number;
   state: number;
   class: number;
@@ -416,10 +325,7 @@ export class ErrorMessageToken extends BaseToken {
   lineNumber: number;
 
   constructor({ number, state, class: clazz, message, serverName, procName, lineNumber }: { number: number, state: number, class: number, message: string, serverName: string, procName: string, lineNumber: number }) {
-    super();
-
-    this.name = 'ERROR';
-    this.event = 'errorMessage';
+    super('ERROR', 'errorMessage');
 
     this.number = number;
     this.state = state;
@@ -431,20 +337,14 @@ export class ErrorMessageToken extends BaseToken {
   }
 }
 
-export class LoginAckToken extends BaseToken {
-  name: 'LOGINACK';
-  event: 'loginack';
-
+export class LoginAckToken extends Token {
   interface: string;
   tdsVersion: string;
   progName: string;
   progVersion: { major: number, minor: number, buildNumHi: number, buildNumLow: number };
 
   constructor({ interface: interfaze, tdsVersion, progName, progVersion }: { interface: LoginAckToken['interface'], tdsVersion: LoginAckToken['tdsVersion'], progName: LoginAckToken['progName'], progVersion: LoginAckToken['progVersion'] }) {
-    super();
-
-    this.name = 'LOGINACK';
-    this.event = 'loginack';
+    super('LOGINACK', 'loginack');
 
     this.interface = interfaze;
     this.tdsVersion = tdsVersion;
@@ -453,62 +353,44 @@ export class LoginAckToken extends BaseToken {
   }
 }
 
-export class NBCRowToken extends BaseToken {
-  name: 'NBCROW';
-  event: 'row';
-  columns: any[];
+export class NBCRowToken extends Token {
+  columns: any;
 
-  constructor(columns: any[]) {
-    super();
+  constructor(columns: any) {
+    super('NBCROW', 'row');
 
-    this.name = 'NBCROW';
-    this.event = 'row';
     this.columns = columns;
   }
 }
 
-export class OrderToken extends BaseToken {
-  name: 'ORDER';
-  event: 'order';
+export class OrderToken extends Token {
   orderColumns: number[];
 
   constructor(orderColumns: number[]) {
-    super();
+    super('ORDER', 'order');
 
-    this.name = 'ORDER';
-    this.event = 'order';
     this.orderColumns = orderColumns;
   }
 }
 
-export class ReturnStatusToken extends BaseToken {
-  name: 'RETURNSTATUS';
-  event: 'returnStatus';
+export class ReturnStatusToken extends Token {
   value: number;
 
   constructor(value: number) {
-    super();
+    super('RETURNSTATUS', 'returnStatus');
 
-    this.name = 'RETURNSTATUS';
-    this.event = 'returnStatus';
     this.value = value;
   }
 }
 
-export class ReturnValueToken extends BaseToken {
-  name: 'RETURNVALUE';
-  event: 'returnValue';
-
+export class ReturnValueToken extends Token {
   paramOrdinal: number;
   paramName: string;
   metadata: Metadata;
   value: unknown;
 
   constructor({ paramOrdinal, paramName, metadata, value }: { paramOrdinal: number, paramName: string, metadata: Metadata, value: unknown }) {
-    super();
-
-    this.name = 'RETURNVALUE';
-    this.event = 'returnValue';
+    super('RETURNVALUE', 'returnValue');
 
     this.paramOrdinal = paramOrdinal;
     this.paramName = paramName;
@@ -517,73 +399,30 @@ export class ReturnValueToken extends BaseToken {
   }
 }
 
-export class RowToken extends BaseToken {
-  name: 'ROW';
-  event: 'row';
-  columns: any[];
+export class RowToken extends Token {
+  columns: any;
 
-  constructor(columns: any[]) {
-    super();
+  constructor(columns: any) {
+    super('ROW', 'row');
 
-    this.name = 'ROW';
-    this.event = 'row';
     this.columns = columns;
   }
 }
 
-export class SSPIToken extends BaseToken {
-  name: 'SSPICHALLENGE';
-  event: 'sspichallenge';
-
+export class SSPIToken extends Token {
   ntlmpacket: any;
   ntlmpacketBuffer: Buffer;
 
   constructor(ntlmpacket: any, ntlmpacketBuffer: Buffer) {
-    super();
-
-    this.name = 'SSPICHALLENGE';
-    this.event = 'sspichallenge';
+    super('SSPICHALLENGE', 'sspichallenge');
 
     this.ntlmpacket = ntlmpacket;
     this.ntlmpacketBuffer = ntlmpacketBuffer;
   }
 }
 
-export class EndOfMessageToken extends BaseToken {
-  name: 'EOM';
-  event: 'endOfMessage';
-
+export class EndOfMessageToken extends Token {
   constructor() {
-    super();
-
-    this.name = 'EOM';
-    this.event = 'endOfMessage';
+    super('EOM', 'endOfMessage');
   }
 }
-
-export type Token = ColMetadataToken
-                  | DoneToken
-                  | DoneInProcToken
-                  | DoneProcToken
-                  | DatabaseEnvChangeToken
-                  | LanguageEnvChangeToken
-                  | CharsetEnvChangeToken
-                  | PacketSizeEnvChangeToken
-                  | BeginTransactionEnvChangeToken
-                  | CommitTransactionEnvChangeToken
-                  | RollbackTransactionEnvChangeToken
-                  | DatabaseMirroringPartnerEnvChangeToken
-                  | ResetConnectionEnvChangeToken
-                  | RoutingEnvChangeToken
-                  | FeatureExtAckToken
-                  | FedAuthInfoToken
-                  | InfoMessageToken
-                  | ErrorMessageToken
-                  | LoginAckToken
-                  | NBCRowToken
-                  | OrderToken
-                  | ReturnStatusToken
-                  | ReturnValueToken
-                  | RowToken
-                  | SSPIToken
-                  | EndOfMessageToken;
