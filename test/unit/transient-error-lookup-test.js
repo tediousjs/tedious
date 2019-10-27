422f48546ef90c20051dac45b4ecb18420dccc72
Arthur Schreiber <schreiber.arthur@googlemail.com>
2019-10-27 21:54:51 +0100

    test: prefer plain `function`s over arrow functions

--- test/unit/transient-error-lookup-test.js
+++ test/unit/transient-error-lookup-test.js
@@ -4,8 +4,8 @@ const assert = require('chai').assert;
 // This test is simply a set of assertions to ensure any additions to or deletions
 // from the list of transient errors is intentional.
 
-describe('Connection configuration validation', () => {
-  it('transient errors', () => {
+describe('Connection configuration validation', function() {
+  it('transient errors', function() {
     const transientErrorLookup = new TransientErrorLookup();
     assert.ok(transientErrorLookup.isTransientError(4060));
     assert.ok(transientErrorLookup.isTransientError(10928));
@@ -15,7 +15,7 @@ describe('Connection configuration validation', () => {
     assert.ok(transientErrorLookup.isTransientError(40613));
   });
 
-  it('not transient error', () => {
+  it('not transient error', function() {
     const transientErrorLookup = new TransientErrorLookup();
     assert.strictEqual(transientErrorLookup.isTransientError(18456), false);
   });
