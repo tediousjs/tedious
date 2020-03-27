2608bd8d6aa6039f31e33cbbad964125dec8d71f
IanChokSimba <ichok@magnitude.com>
2020-03-27 10:36:31 -0700

    refactor: add if condition to request

--- src/connection.ts
+++ src/connection.ts
@@ -1971,8 +1971,11 @@ class Connection extends EventEmitter {
 
     tokenStreamParser.on('errorMessage', (token) => {
       this.emit('errorMessage', token);
-      const request = this.request as Request;
-      request.emit('errorMessage', token.name, token.message);
+      if (this.request) {
+        const request = this.request as Request;
+        request.emit('errorMessage', token.name, token.message);
+      }
+
 
       if (this.loggedIn) {
         const request = this.request;
