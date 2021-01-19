990370c29131ce79ff2ed7e6eb98ffb0a27c275f
Arthur Schreiber <schreiber.arthur@googlemail.com>
2021-01-19 17:02:19 +0000

    refactor: make `ParallelSendStrategy` use one socket per address

--- src/sender.ts
+++ src/sender.ts
@@ -23,48 +23,23 @@ export class ParallelSendStrategy {
   port: number;
   request: Buffer;
 
-  socketV4: dgram.Socket | null;
-  socketV6: dgram.Socket | null;
-
-  onMessage: ((message: Buffer) => void) | null;
-  onError: ((err: Error) => void) | null;
   signal: AbortSignal;
 
   constructor(addresses: dns.LookupAddress[], port: number, signal: AbortSignal, request: Buffer) {
     this.addresses = addresses;
     this.port = port;
     this.request = request;
-
-    this.socketV4 = null;
-    this.socketV6 = null;
-    this.onError = null;
-    this.onMessage = null;
-
     this.signal = signal;
   }
 
-  clearSockets() {
-    const clearSocket = (socket: dgram.Socket, onError: (err: Error) => void, onMessage: (message: Buffer) => void) => {
-      socket.removeListener('error', onError);
-      socket.removeListener('message', onMessage);
-      socket.close();
-    };
-
-    if (this.socketV4) {
-      clearSocket(this.socketV4, this.onError!, this.onMessage!);
-      this.socketV4 = null;
-    }
-
-    if (this.socketV6) {
-      clearSocket(this.socketV6, this.onError!, this.onMessage!);
-      this.socketV6 = null;
-    }
-  }
-
   send(cb: (error: Error | null, message?: Buffer) => void) {
-    if (this.signal.aborted) {
+    const signal = this.signal;
+
+    if (signal.aborted) {
       return cb(new AbortError());
     }
+
+    const sockets: dgram.Socket[] = [];
 
     let errorCount = 0;
 
@@ -72,62 +47,45 @@ export class ParallelSendStrategy {
       errorCount++;
 
       if (errorCount === this.addresses.length) {
-        this.signal.removeEventListener('abort', onAbort);
-        this.clearSockets();
+        signal.removeEventListener('abort', onAbort);
+        clearSockets();
 
         cb(err);
       }
     };
 
     const onMessage = (message: Buffer) => {
-      this.signal.removeEventListener('abort', onAbort);
-      this.clearSockets();
+      signal.removeEventListener('abort', onAbort);
+      clearSockets();
 
       cb(null, message);
     };
 
     const onAbort = () => {
-      this.clearSockets();
+      clearSockets();
 
       cb(new AbortError());
     };
 
-    this.signal.addEventListener('abort', onAbort, { once: true });
-
-    const createDgramSocket = (udpType: 'udp4' | 'udp6', onError: (err: Error) => void, onMessage: (message: Buffer) => void) => {
-      const socket = dgram.createSocket(udpType);
-
-      socket.on('error', onError);
-      socket.on('message', onMessage);
-      return socket;
+    const clearSockets = () => {
+      for (const socket of sockets) {
+        socket.removeListener('error', onError);
+        socket.removeListener('message', onMessage);
+        socket.close();
+      }
     };
 
+    signal.addEventListener('abort', onAbort, { once: true });
+
     for (let j = 0; j < this.addresses.length; j++) {
-      const udpTypeV4 = 'udp4';
-      const udpTypeV6 = 'udp6';
+      const udpType = this.addresses[j].family === 6 ? 'udp6' : 'udp4';
 
-      const udpType = this.addresses[j].family === 6 ? udpTypeV6 : udpTypeV4;
-      let socket;
-
-      if (udpType === udpTypeV4) {
-        if (!this.socketV4) {
-          this.socketV4 = createDgramSocket(udpTypeV4, onError, onMessage);
-        }
-
-        socket = this.socketV4;
-      } else {
-        if (!this.socketV6) {
-          this.socketV6 = createDgramSocket(udpTypeV6, onError, onMessage);
-        }
-
-        socket = this.socketV6;
-      }
-
+      const socket = dgram.createSocket(udpType);
+      sockets.push(socket);
+      socket.on('error', onError);
+      socket.on('message', onMessage);
       socket.send(this.request, 0, this.request.length, this.port, this.addresses[j].address);
     }
-
-    this.onError = onError;
-    this.onMessage = onMessage;
   }
 }
 
