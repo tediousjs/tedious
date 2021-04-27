9247b363126d8f0b1d54bd718a2af4d0be6408a3
Ian Chok <ichok@magnitude.com>
2021-04-27 15:29:41 -0700

    chore: add always-encrypte methods folder

---
+++ src/always-encrypted/utils.ts
@@ -0,0 +1,16 @@
+// This code is based on the `mssql-jdbc` library published under the conditions of MIT license.
+// Copyright (c) 2019 Microsoft Corporation
+
+import { SQLServerStatementColumnEncryptionSetting } from './types';
+
+export const shouldHonorAE = (stmtColumnEncryptionSetting: SQLServerStatementColumnEncryptionSetting, columnEncryptionSetting: boolean): boolean => {
+  switch (stmtColumnEncryptionSetting) {
+    case SQLServerStatementColumnEncryptionSetting.Disabled:
+    case SQLServerStatementColumnEncryptionSetting.ResultSetOnly:
+      return false;
+    case SQLServerStatementColumnEncryptionSetting.Enabled:
+      return true;
+    default:
+      return columnEncryptionSetting;
+  }
+};
