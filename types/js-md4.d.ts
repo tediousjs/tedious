be73bd855c989975e00cc17edd75c6940eca9469
mShan0 <96149598+mShan0@users.noreply.github.com>
2022-06-28 09:35:13 -0700

    fix TS definition

--- types/js-md4.d.ts
+++ types/js-md4.d.ts
@@ -1,6 +1,6 @@
 declare module 'js-md4' {
   declare const md4: {
-    arrayBuffer(message: string | ArrayBuffer): string;
+    arrayBuffer(message: string | ArrayBuffer): ArrayBuffer;
   };
 
   export = md4;
