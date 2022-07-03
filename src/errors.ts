658e1ad27af1fc8ca76303929ba6dec984167f00
Arthur Schreiber <schreiber.arthur@googlemail.com>
2022-07-03 18:59:01 +0000

    refactor: extract common `AbortError`

--- src/errors.ts
+++ src/errors.ts
@@ -26,3 +26,14 @@ export class RequestError extends Error {
     this.code = code;
   }
 }
+
+export class AbortError extends Error {
+  code: string;
+
+  constructor() {
+    super('The operation was aborted');
+
+    this.code = 'ABORT_ERR';
+    this.name = 'AbortError';
+  }
+}
--- src/instance-lookup.ts
+++ src/instance-lookup.ts
@@ -1,5 +1,6 @@
 import dns from 'dns';
 import { AbortController, AbortSignal } from 'node-abort-controller';
+import { AbortError } from './errors';
 
 import { Sender } from './sender';
 
@@ -10,17 +11,6 @@ const RETRIES = 3;
 const MYSTERY_HEADER_LENGTH = 3;
 
 type LookupFunction = (hostname: string, options: dns.LookupAllOptions, callback: (err: NodeJS.ErrnoException | null, addresses: dns.LookupAddress[]) => void) => void;
-
-class AbortError extends Error {
-  code: string;
-
-  constructor() {
-    super('The operation was aborted');
-
-    this.code = 'ABORT_ERR';
-    this.name = 'AbortError';
-  }
-}
 
 // Most of the functionality has been determined from from jTDS's MSSqlServerInfo class.
 export class InstanceLookup {
