718145ac4ea154bfd877af8aa8c33144ae150577
mShan0 <96149598+mShan0@users.noreply.github.com>
2022-08-04 13:14:53 -0700

    refactor: refactor `OrderToken`

--- src/token/order-token-parser.ts
+++ src/token/order-token-parser.ts
@@ -3,30 +3,52 @@ import Parser, { ParserOptions } from './stream-parser';
 
 import { OrderToken } from './token';
 
+class NotEnoughDataError extends Error { }
+
+let offset: number;
+
+function checkDataLength(buffer: Buffer, numBytes: number): void {
+  if (buffer.length < offset + numBytes) {
+    throw new NotEnoughDataError();
+  }
+}
+
+function readUInt16LE(parser: Parser): number {
+  const numBytes = 2;
+  checkDataLength(parser.buffer, numBytes);
+  const data = parser.buffer.readUInt16LE(offset);
+  offset += numBytes;
+  return data;
+}
+
+function parseToken(parser: Parser): OrderToken {
+  offset = parser.position;
+
+  const length = readUInt16LE(parser);
+  const columnCount = length / 2;
+  const orderColumns: number[] = [];
+
+  for (let i = 0; i < columnCount; i++) {
+    const column = readUInt16LE(parser);
+    orderColumns.push(column);
+  }
+
+  parser.position = offset;
+  return new OrderToken(orderColumns);
+}
+
 function orderParser(parser: Parser, _options: ParserOptions, callback: (token: OrderToken) => void) {
-  parser.readUInt16LE((length) => {
-    const columnCount = length / 2;
-    const orderColumns: number[] = [];
-
-    let i = 0;
-    function next(done: () => void) {
-      if (i === columnCount) {
-        return done();
-      }
-
-      parser.readUInt16LE((column) => {
-        orderColumns.push(column);
-
-        i++;
-
-        next(done);
+  let data!: OrderToken;
+  try {
+    data = parseToken(parser);
+  } catch (err) {
+    if (err instanceof NotEnoughDataError) {
+      return parser.suspend(() => {
+        orderParser(parser, _options, callback);
       });
     }
-
-    next(() => {
-      callback(new OrderToken(orderColumns));
-    });
-  });
+  }
+  callback(data);
 }
 
 export default orderParser;
