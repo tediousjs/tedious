b8faa65f54af88cf13c7a58ed99ac07bd4d9e353
mShan0 <96149598+mShan0@users.noreply.github.com>
2022-12-17 06:15:08 -0800

    refactor: refactor `fedauth-info-parser` (#1508)

--- src/token/fedauth-info-parser.ts
+++ src/token/fedauth-info-parser.ts
@@ -1,49 +1,79 @@
 import Parser, { ParserOptions } from './stream-parser';
 import { FedAuthInfoToken } from './token';
 
+import { LVarbyte, Map, Record, UInt8, UInt32LE } from '../parser';
+
 const FEDAUTHINFOID = {
   STSURL: 0x01,
   SPN: 0x02
 };
 
-function fedAuthInfoParser(parser: Parser, _options: ParserOptions, callback: (token: FedAuthInfoToken) => void) {
-  parser.readUInt32LE((tokenLength) => {
-    parser.readBuffer(tokenLength, (data) => {
-      let spn: string | undefined, stsurl: string | undefined;
+interface FedAuthInfoOpt {
+  fedAuthInfoID: number;
+  fedAuthInfoDataLen: number;
+  fedAuthInfoDataOffset: number;
+}
 
-      let offset = 0;
-
-      const countOfInfoIDs = data.readUInt32LE(offset);
-      offset += 4;
-
-      for (let i = 0; i < countOfInfoIDs; i++) {
-        const fedauthInfoID = data.readUInt8(offset);
-        offset += 1;
-
-        const fedAuthInfoDataLen = data.readUInt32LE(offset);
-        offset += 4;
-
-        const fedAuthInfoDataOffset = data.readUInt32LE(offset);
-        offset += 4;
-
-        switch (fedauthInfoID) {
-          case FEDAUTHINFOID.SPN:
-            spn = data.toString('ucs2', fedAuthInfoDataOffset, fedAuthInfoDataOffset + fedAuthInfoDataLen);
-            break;
-
-          case FEDAUTHINFOID.STSURL:
-            stsurl = data.toString('ucs2', fedAuthInfoDataOffset, fedAuthInfoDataOffset + fedAuthInfoDataLen);
-            break;
-
-          // ignoring unknown fedauthinfo options
-          default:
-            break;
-        }
-      }
-
-      callback(new FedAuthInfoToken(spn, stsurl));
+class FedAuthInfoOptParser extends Record<FedAuthInfoOpt> {
+  constructor() {
+    super({
+      fedAuthInfoID: new UInt8(),
+      fedAuthInfoDataLen: new UInt32LE(),
+      fedAuthInfoDataOffset: new UInt32LE()
     });
-  });
+  }
+}
+
+function parseTokenData(buffer: Buffer) {
+  let offset = 0;
+  let spn: string | undefined, stsurl: string | undefined;
+  const result = new UInt32LE().parse(buffer, offset);
+
+  if (!result.done) {
+    throw new Error('Parsing error');
+  }
+
+  const countOfInfoIDs = result.value;
+  offset = result.offset;
+  for (let i = 0; i < countOfInfoIDs!; i++) {
+    const parser = new FedAuthInfoOptParser();
+    const result = parser.parse(buffer, offset);
+
+    if (!result.done) {
+      throw new Error('Parsing error');
+    }
+
+    const info = result.value;
+
+    switch (info.fedAuthInfoID) {
+      case FEDAUTHINFOID.SPN:
+        spn = buffer.toString('ucs2', info.fedAuthInfoDataOffset, info.fedAuthInfoDataOffset + info.fedAuthInfoDataLen);
+        break;
+
+      case FEDAUTHINFOID.STSURL:
+        stsurl = buffer.toString('ucs2', info.fedAuthInfoDataOffset, info.fedAuthInfoDataOffset + info.fedAuthInfoDataLen);
+        break;
+
+      // ignoring unknown fedauthinfo options
+      default:
+        break;
+    }
+    offset = result.offset;
+  }
+
+  return new FedAuthInfoToken(spn, stsurl);
+}
+
+export class FedAuthInfoTokenParser extends Map<Buffer, FedAuthInfoToken> {
+  constructor() {
+    super(new LVarbyte(), (buffer) => {
+      return parseTokenData(buffer);
+    });
+  }
+}
+
+function fedAuthInfoParser(parser: Parser, options: ParserOptions, callback: (token: FedAuthInfoToken) => void) {
+  parser.execParser(FedAuthInfoTokenParser, callback);
 }
 
 export default fedAuthInfoParser;
