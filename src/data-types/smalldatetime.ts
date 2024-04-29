3a58a6176ccf67ff264701c6e396725d615496a7
rockymadden <git@rockymadden.com>
2024-04-29 14:26:14 -0600

    fix: add ref for SmallDateTime range change

--- src/data-types/smalldatetime.ts
+++ src/data-types/smalldatetime.ts
@@ -79,6 +79,7 @@ const SmallDateTime: DataType = {
 
     if (year === 2079) {
       // Month is 0-indexed, i.e. Jan = 0, Dec = 11
+      // See: https://learn.microsoft.com/en-us/sql/t-sql/data-types/smalldatetime-transact-sql?view=sql-server-ver16
       if (month > 5 || (month === 5 && date > 6)) {
         throw new TypeError('Out of range.');
       }
