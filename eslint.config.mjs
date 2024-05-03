bec062d9092e0eb564ffbbd2b9ac38077145ab07
Elliot Huffman <elhuff@mootinc.com>
2024-05-03 15:22:59 -0400

    Migrate to Flat Config
    Change the packages to TypeScript Lint to the next gen package.
    Migrate all the settings over from the existing ESLint config.
    Remove `deprecated` and `removed` rules.
    Enable the typescript strict type checked and stylistic checked rule collections.
    Update lint config to ES 2021 instead of ES6.
    Add the new ESLint config to the typescript config so that it is aware if it and can provide typing services in the config file.

---
+++ eslint.config.mjs
@@ -0,0 +1,87 @@
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        'ignores': [
+            "/src/**/*.d.ts",
+            "eslint.config.mjs"
+        ]
+    },
+    {
+        'languageOptions': {
+            'globals': {
+                ...globals.mocha,
+                ...globals.node,
+                ...globals.es2021
+            },
+            'parserOptions': { 'project': true }
+        },
+        'linterOptions': { 'reportUnusedDisableDirectives': true },
+        'rules': {
+            "@typescript-eslint/no-use-before-define": ["error", {
+                "classes": true,
+                "functions": false,
+                "variables": false
+            }],
+            "@typescript-eslint/return-await": ["error", "always"],
+            "no-var": "error",
+            "prefer-const": "error",
+            "prefer-rest-params": "error",
+            "prefer-spread": "error",
+            "accessor-pairs": "error",
+            "array-callback-return": "error",
+            "dot-notation": "error",
+            "eqeqeq": ["error", "smart"],
+            "func-name-matching": "error",
+            "func-style": ["error", "declaration", { "allowArrowFunctions": true }],
+            "no-delete-var": "error",
+            "no-duplicate-imports": "error",
+            "no-extra-boolean-cast": "error",
+            "no-lonely-if": "error",
+            "no-octal": "error",
+            "no-proto": "error",
+            "no-redeclare": "error",
+            "no-restricted-syntax": ["error",
+                {
+                    "selector": "CallExpression[callee.name='setTimeout'][arguments.length<2]",
+                    "message": "`setTimeout()` must be invoked with at least two arguments.",
+                },
+                {
+                    "selector": "CallExpression[callee.name='setInterval'][arguments.length<2]",
+                    "message": "`setInterval()` must be invoked with at least two arguments.",
+                },
+                {
+                    "selector": "ThrowStatement > CallExpression[callee.name=/Error$/]",
+                    "message": "Use `new` keyword when throwing an `Error`.",
+                }
+            ],
+            "no-self-compare": "error",
+            "no-template-curly-in-string": "error",
+            "no-throw-literal": "error",
+            "no-undef-init": "error",
+            "no-unused-labels": "error",
+            "no-use-before-define": ["error", {
+                "classes": true,
+                "functions": false,
+                "variables": false
+            }],
+            "no-useless-call": "error",
+            "no-useless-catch": "error",
+            "no-useless-concat": "error",
+            "no-useless-constructor": "error",
+            "no-useless-escape": "error",
+            "no-useless-return": "error",
+            "no-void": "error",
+            "no-with": "error",
+            "one-var": ["error", { "initialized": "never" }],
+            "prefer-const": ["error", { "ignoreReadBeforeAssign": true }],
+            "symbol-description": "error",
+            "unicode-bom": "error"
+        }
+    }
+);
