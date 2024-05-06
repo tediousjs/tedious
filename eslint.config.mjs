f0f51ae04ac862d77207fd3e4b1d5a003021da16
Elliot Huffman <elhuff@mootinc.com>
2024-05-06 18:52:03 -0400

    Disable New Rulesets
    These will be enabled in future PRs...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,6 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
     {
         'ignores': [
             "/src/**/*.d.ts",
