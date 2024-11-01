f209f0e4d021b902665e957c582249fbfbf41eba
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2024-11-01 12:12:50 -0400

    re-added database mirroring handler for login7tokenhandler

--- src/token/handler.ts
+++ src/token/handler.ts
@@ -284,6 +284,10 @@ export class Login7TokenHandler extends TokenHandler {
     this.connection.emit('databaseChange', token.newValue);
   }
 
+  onDatabaseMirroringPartner(token: DatabaseMirroringPartnerEnvChangeToken) {
+    this.connection.emit('databaseMirroringPartner', token.newValue);
+  }
+
   onLanguageChange(token: LanguageEnvChangeToken) {
     this.connection.emit('languageChange', token.newValue);
   }
