dfe3d4eee660e77f100ad4b4c44feb270b07047f
Tim Vallancourt <tim@hightouch.io>
2025-01-22 15:33:49 -0800

    handle empty instance

--- src/connection.ts
+++ src/connection.ts
@@ -2485,7 +2485,9 @@ class Connection extends EventEmitter {
     }
 
     payload.hostname = this.config.options.workstationId || os.hostname();
-    payload.serverName = this.routingData ? `${this.routingData.server}\\${this.routingData.instance}` : this.config.server;
+    payload.serverName = this.routingData ?
+      `${this.routingData.server}${this.routingData.instance ? '\\' + this.routingData.instance : ''}` :
+      this.config.server;
     payload.appName = this.config.options.appName || 'Tedious';
     payload.libraryName = libraryName;
     payload.language = this.config.options.language;
